c385eda92c00d9e9dd71647ee090a20ff6f27d05
samet tunay <samet.tunay12@gmail.com>
2023-03-08 20:51:24 +0300

    Try Catch

---
+++ C#/Basics/TryAndCatch/TryAndCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.WriteLine("Bir sayı giriniz: ");
+    int sayi1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Bir sayı daha giriniz: ");
+    int sayi2 = int.Parse(Console.ReadLine());
+    int bolum = sayi1 / sayi2;
+
+    Console.WriteLine($"Bölme işleminin sonucu: {bolum}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Sayı diyoruz kardeş!");
+}
+catch (DivideByZeroException) 
+{
+    Console.WriteLine("Tam sayılar 0'a bölünemez!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Bu satır mutlaka çalışacak!");
+}
+
