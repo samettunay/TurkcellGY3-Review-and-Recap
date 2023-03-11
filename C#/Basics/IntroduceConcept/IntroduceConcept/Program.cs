04603b3b0be54a1af387d98658fac6a3065df5b6
samet tunay <samet.tunay12@gmail.com>
2023-03-11 17:47:15 +0300

    Introduce Concept

---
+++ C#/Basics/IntroduceConcept/IntroduceConcept/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroduceConcept
+{ //Scope
+    internal class Program
+    { //Scope
+        static void Main(string[] args)
+        { //Scope
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
