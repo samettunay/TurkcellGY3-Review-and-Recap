4cbaef6522f04084e04d7f4358222b26c1d8f3ff
samet tunay <samet.tunay12@gmail.com>
2023-03-17 20:36:34 +0300

    Polymorphism

---
+++ C#/OOP/OOPOverview/Polymorphism/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Polymorphism;
+
+Console.WriteLine("Hello, World!");
+
+Asci asci = new Asci();
+asci.Pisir(new Kurufasulye() { Ad = "Kurufasülye" });
+asci.Pisir(new Baklava() { Ad = "Baklava" });
