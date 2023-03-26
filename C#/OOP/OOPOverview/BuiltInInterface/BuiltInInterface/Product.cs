8887170eccdcd2c3fb4ed7964b9b9ccb09877e06
samet tunay <samet.tunay12@gmail.com>
2023-03-26 11:02:22 +0300

    Built in Interface

---
+++ C#/OOP/OOPOverview/BuiltInInterface/BuiltInInterface/Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltInInterface
+{
+    public class Product : IComparable<Product>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public int CompareTo(Product? other)
+        {
+            if (Price > other.Price)
+            {
+                return 1;
+            }
+            else if (Price < other.Price)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    public class ProductCollection : IEnumerable<Product>
+    {
+        private List<Product> products = new List<Product>();
+
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Clear()
+        {
+            products.Clear();
+        }
+
+        public void SortProducts()
+        {
+            products.Sort();
+        }
+
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            foreach (var product in products)
+            {
+                yield return product;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
