de34292c0103043bfed2dac2d706303364196b49
samet tunay <samet.tunay12@gmail.com>
2023-03-26 11:19:20 +0300

    SRP and OCP

---
+++ C#/OOP/OOPOverview/SOLID/SingleResponsibility/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class ProductService
+    {
+        public int AddProduct(string name, decimal price)
+        {
+            //Environment Values olmalı:
+            string connectionString = "Data Source=(localdb)\\Mssqllocaldb;Initial Catalog=Northwind;Integrated Security=True";
+            string commandText = "Insert into Products (ProductName, UnitPrice) values (@name,@price)";
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+
+            parameters.Add("@name", name);
+            parameters.Add("@price", price);
+
+            DataAccess dataAccess = new DataAccess(connectionString);
+            var affectedRows = dataAccess.ExecuteNonQuery(commandText, parameters);
+
+            return affectedRows;
+        }
+
+        public int UpdateProduct(string name, decimal price)
+        {
+            return 0;
+        }
+
+        public void SendInfoMailToProductOwner(string mail)
+        {
+
+        }
+
+
+    }
+}
---
+++ C#/OOP/OOPOverview/SOLID/SingleResponsibility/Program.cs
@@ -0,0 +1,12 @@
+﻿using SingleResponsibility;
+
+void buttonAddProduct_Click(string name, decimal price)
+{
+    ProductService productService = new ProductService();
+    string name1 = name;
+    decimal price1 = price;
+    var affectedRows = productService.AddProduct(name, price);
+
+    string message = affectedRows > 0 ? "Başarılı" : "İşlem yapılamadı";
+    Console.WriteLine(message);
+}
