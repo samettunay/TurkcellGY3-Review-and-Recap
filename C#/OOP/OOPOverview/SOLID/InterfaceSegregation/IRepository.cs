fc90fef6dce66e0fe0ed66b58b47181014dfbd8e
samet tunay <samet.tunay12@gmail.com>
2023-03-28 12:14:00 +0300

    Interface Segregation

---
+++ C#/OOP/OOPOverview/SOLID/InterfaceSegregation/IRepository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+
+    interface IProductRepository : IRepository<Product>
+    {
+        IEnumerable<Product> GetProductsByName(string name);
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductRepository : IProductRepository
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProductsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CategoryRepository : IRepository<Category>
+    {
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
