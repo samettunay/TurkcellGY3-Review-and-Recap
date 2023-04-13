500e776dbd50bb7521720aac8636d30874abb5af
samet tunay <samet.tunay12@gmail.com>
2023-04-13 17:56:59 +0300

    Fetching, Events, Validation

---
+++ HTML-CSS-JS/usingJavaScript/validate.js
@@ -0,0 +1,28 @@
+function validate(params) {
+    var requiredInputs = document.getElementsByClassName("required");
+    let isValid = true;
+
+    for (let index = 0; index < requiredInputs.length; index++) {
+        const element = requiredInputs[index];
+        isValid = isRequired(element) && isValid;
+    }
+    return isValid;
+}
+
+function isRequired(element) {
+    let isAvailable = true;
+    let value = element.value;
+    if (value === "") {
+        let span = element.getAttribute("data-target");
+        let message = element.getAttribute("data-message");
+
+        element.addEventListener("change", function () {
+        document.getElementById(span).innerText = "";
+        });
+
+        document.getElementById(span).innerText = message;
+        isAvailable = false;
+    }
+
+    return isAvailable;
+}
