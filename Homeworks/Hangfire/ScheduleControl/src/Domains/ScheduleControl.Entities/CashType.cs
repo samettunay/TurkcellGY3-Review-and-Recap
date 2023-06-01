f5be5cc03336953b0e88f1620c54271d1382fa98
Samet Tunay <79511355+samettunay@users.noreply.github.com>
2023-06-01 21:04:44 +0300

    File layout

---
+++ Homeworks/Hangfire/ScheduleControl/src/Domains/ScheduleControl.Entities/CashType.cs
@@ -0,0 +1,8 @@
+﻿namespace ScheduleControl.Entities
+{
+    public class CashType : IEntity
+    {
+        public int CashTypeId { get; set; }
+        public string CashTypeName { get; set; }
+    }
+}
---
+++ Homeworks/Hangfire/ScheduleControl/src/Domains/ScheduleControl.Entities/Cashbox.cs
@@ -0,0 +1,9 @@
+﻿namespace ScheduleControl.Entities
+{
+    public class Cashbox : IEntity
+    {
+        public int CashboxId { get; set; }
+        public int CashTypeId { get; set; }
+        public decimal TotalQuantity { get; set; }
+    }
+}
