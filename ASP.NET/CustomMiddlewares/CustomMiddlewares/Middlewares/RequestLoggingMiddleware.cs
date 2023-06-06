06a84c8dedfac379ea7742c8381b9c05a5bb8fd7
Samet Tunay <79511355+samettunay@users.noreply.github.com>
2023-06-06 18:27:14 +0300

    Bad words middleware

--- ASP.NET/CustomMiddlewares/CustomMiddlewares/Middlewares/RequestLoggingMiddleware.cs
+++ ASP.NET/CustomMiddlewares/CustomMiddlewares/Middlewares/RequestLoggingMiddleware.cs
@@ -11,20 +11,41 @@
             _logger = logger;
         }
 
-        public async Task Invoke(HttpContext httpContext) 
+        public async Task Invoke(HttpContext httpContext)
         {
-            _logger.LogInformation($"{DateTime.Now} anında gelen request methodu: {httpContext.Request.Method}. Talebin gönderildiği adresi: {httpContext.Request.Path} ");
+            /*
+        * Bu middleware'in amacı; API'ye gelen her request'i yakalamak ve bilgi vermektir.
+        * 
+        */
+
+            // Gelen isteğin bilgilerini loglamak için ILogger kullanılıyor.
+            _logger.LogInformation($"{DateTime.Now} anında Gelen request metodu: {httpContext.Request.Method}. Talebin gönderildiği adres: {httpContext.Request.Path}.");
+
             var responseBodyStream = httpContext.Response.Body;
+
+            // Yanıtın bir kopyasını tutmak için yeni bir bellek akışı oluşturuluyor.
             var responseStream = new MemoryStream();
+
+            // Yanıt akışı, oluşturulan bellek akışına yönlendiriliyor.
             httpContext.Response.Body = responseStream;
+
+            // Sonraki middleware'i çağırmak ve işlem zincirinin devamını sağlamak için await kullanılıyor.
             await _next(httpContext);
+
+            // Bellek akışını başa sararak yanıtın okunabilir hale getirilmesi sağlanıyor.
             responseStream.Seek(0, SeekOrigin.Begin);
+
+            // Yanıtın içeriğini okumak için bir StreamReader kullanılıyor.
             var responseBody = new StreamReader(responseStream).ReadToEnd();
-            _logger.LogInformation($"{DateTime.Now} Response oluştu: {httpContext.Response.StatusCode} --> \n {responseBody}");
 
+            _logger.LogInformation($"{DateTime.Now} anında Response oluştu....");
+            _logger.LogInformation($"Oluşan response: {httpContext.Response.StatusCode} -> {responseBody} ");
+
+            // Bellek akışını başa sararak yanıtın orijinal akışa kopyalanması sağlanıyor.
             responseStream.Seek(0, SeekOrigin.Begin);
-            await responseStream.CopyToAsync(responseBodyStream);
-        }
 
+            await responseStream.CopyToAsync(responseBodyStream);
+
+        }
     }
 }
