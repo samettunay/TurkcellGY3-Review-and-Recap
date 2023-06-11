af099f45c2f7dae1af25b69a03262f72c8123f6d
Samet Tunay <79511355+samettunay@users.noreply.github.com>
2023-06-11 21:29:29 +0300

    Hangfire

--- Homeworks/Hangfire/ScheduleControl/src/Domains/ScheduleControl.Entities/Cashbox.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScheduleControl.Entities
-{
-    public class Cashbox : IEntity
-    {
-        public int CashboxId { get; set; }
-        public int CashTypeId { get; set; }
-        public decimal TotalQuantity { get; set; }
-    }
-}
